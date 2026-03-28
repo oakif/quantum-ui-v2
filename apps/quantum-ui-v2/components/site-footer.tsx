aa8c7f07045f012ceb4fef330979471983076dbb
Oion Akif <48932412+oakif@users.noreply.github.com>
2026-03-28 22:40:19 +0000

    update footer attribution

--- apps/quantum-ui-v2/components/site-footer.tsx
+++ apps/quantum-ui-v2/components/site-footer.tsx
@@ -6,32 +6,14 @@ export function SiteFooter() {
       <div className="container-wrapper px-4 xl:px-6">
         <div className="flex h-(--footer-height) items-center justify-between">
           <div className="w-full px-1 text-center text-xs leading-loose text-muted-foreground sm:text-sm">
-            Built by{" "}
-            <a
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              shadcn
-            </a>{" "}
-            at{" "}
-            <a
-              href="https://vercel.com/new?utm_source=shadcn_site&utm_medium=web&utm_campaign=docs_cta_deploy_now_callout"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Vercel
-            </a>
-            . The source code is available on{" "}
+            Created by Oion Akif based on{" "}
             <a
               href={siteConfig.links.github}
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              GitHub
+              shadcn/ui
             </a>
             .
           </div>
